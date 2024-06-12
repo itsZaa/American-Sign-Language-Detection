12474d9c4d9c86901ca8d823f5bbd7abdba60f7e
kevinnp1093 <if-21009@students.ithb.ac.id>
2024-06-12 21:19:48 +0700

    coba link text
    nyoba link login ke register dan sebaliknya

--- app/src/main/java/com/example/tubesrpll/SignIn.kt
+++ app/src/main/java/com/example/tubesrpll/SignIn.kt
@@ -2,6 +2,10 @@ package com.example.tubesrpll
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -44,5 +48,21 @@ class SignIn : AppCompatActivity() {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
             }
         }
+
+        val textView = findViewById<TextView>(R.id.textView7)
+
+        val text = "Don’t have accounts? Register Now!"
+        val spannableString = SpannableString(text)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@SignIn, SignUp::class.java)
+                startActivity(intent)
+            }
+        }
+
+        spannableString.setSpan(clickableSpan, 10, 18, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = spannableString
+        textView.movementMethod = android.text.method.LinkMovementMethod.getInstance()
     }
 }
--- app/src/main/java/com/example/tubesrpll/SignUp.kt
+++ app/src/main/java/com/example/tubesrpll/SignUp.kt
@@ -1,6 +1,12 @@
 package com.example.tubesrpll
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,21 @@ class SignUp : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textView = findViewById<TextView>(R.id.textView7)
+
+        val text = "Don’t have accounts? Register Now!"
+        val spannableString = SpannableString(text)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@SignUp, SignIn::class.java)
+                startActivity(intent)
+            }
+        }
+
+        spannableString.setSpan(clickableSpan, 10, 18, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = spannableString
+        textView.movementMethod = android.text.method.LinkMovementMethod.getInstance()
     }
 }
