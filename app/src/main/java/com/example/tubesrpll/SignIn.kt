4cb369c7d4d71d2780adf816817164c445d541e5
Akevin12 <kevinthony74@gmail.com>
2024-06-12 21:11:53 +0700

    backend login

--- app/src/main/java/com/example/tubesrpll/SignIn.kt
+++ app/src/main/java/com/example/tubesrpll/SignIn.kt
@@ -1,20 +1,48 @@
 package com.example.tubesrpll
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class SignIn : AppCompatActivity() {
+
+    private lateinit var emailEt: EditText
+    private lateinit var passEt: EditText
+    private lateinit var signInButton: Button
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signin)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_sign_in)
+
+        emailEt = findViewById(R.id.textInputEditText)
+        passEt = findViewById(R.id.editTextPassword)
+        signInButton = findViewById(R.id.button6)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        signInButton.setOnClickListener {
+            val email = emailEt.text.toString()
+            val pass = passEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity2::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
